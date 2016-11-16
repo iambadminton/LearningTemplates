c742acee85d4b793aa7550599a0a7bbec3f893e1
a.shipulin <sfmrs@mail.ru>
2016-11-16 09:53:16 +0300

    Rejected commit: Default

--- src/Fabric/ConcreteCreatorA.java
+++ src/Fabric/ConcreteCreatorA.java
@@ -2,7 +2,11 @@ package Fabric;
 
 /**
  * Created by a.shipulin on 02.11.16.
+ * &&&&&&&&&&&&&&&&&&&&&&&&&&
  */
+
+
+
 public class ConcreteCreatorA extends Creator {
     @Override
     public Product factoryMethod() {
