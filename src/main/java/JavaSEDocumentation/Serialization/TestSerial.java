caf98a41cc3b266531ed5ba42115a35565631e27
a.shipulin <sfmrs@mail.ru>
2017-09-06 16:58:38 +0300

    Rejected commit: Default

--- src/main/java/JavaSEDocumentation/Serialization/TestSerial.java
+++ src/main/java/JavaSEDocumentation/Serialization/TestSerial.java
@@ -6,7 +6,11 @@ import java.io.*;
  * Created by a.shipulin on 05.09.17.
  */
 public class TestSerial implements Serializable {
-    public static void main(String[] args) throws IOException {
+    public byte version = 100;
+    public byte count = 0;
+    private static final long serialVersionUID = 656565L;
+
+    /*public static void main(String[] args) throws IOException {
         FileOutputStream fos = new FileOutputStream("temp.out");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         System.out.println("====> " + oos.toString());
@@ -16,5 +20,12 @@ public class TestSerial implements Serializable {
         oos.writeObject(ts);
         oos.flush();
         oos.close();
+    }*/
+
+    public static void main(String args[]) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream("temp.out");
+        ObjectInputStream oin = new ObjectInputStream(fis);
+        TestSerial ts = (TestSerial) oin.readObject();
+        System.out.println("version="+ts.version);
     }
 }
