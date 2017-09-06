3790eebb5ae7dcd99b55d3f4b0d2f5cb0e03b81e
a.shipulin <sfmrs@mail.ru>
2017-09-06 16:59:27 +0300

    Rejected commit: Default

---
+++ src/main/java/JavaSEDocumentation/Serialization/TestPrimitiveJava.java
@@ -0,0 +1,11 @@
+package JavaSEDocumentation.Serialization;
+
+/**
+ * Created by a.shipulin on 05.09.17.
+ */
+public class TestPrimitiveJava {
+    public static void main(String[] args) {
+        Integer a = 123;
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
