1553e8b3127c9f2377b501cbf076f597d8be4294
a.shipulin <sfmrs@mail.ru>
2018-02-21 16:45:43 +0300

    Rejected commit: Default

---
+++ src/main/java/WorkWithRobot/Example.java
@@ -0,0 +1,83 @@
+package WorkWithRobot;
+
+/**
+ * Created by a.shipulin on 21.02.18.
+ */
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Example extends TelegramLongPollingBot{
+    public static void main(String[] args) {
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+
+
+        try {
+            botapi.registerBot(new Example());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void mySend() {
+        // -->
+        /*SendMessage s = new SendMessage();
+        s.setText("test!!!");
+        try { //Чтобы не крашнулась программа при вылете Exception
+            sendMessage(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }*/
+        SendMessage s = new SendMessage();
+
+        //s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText("test");
+        // <--
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return "CKBRANINOUT";
+        //возвращаем юзера
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        // Тут будет то, что выполняется при получении сообщения
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "This is my test bot!");
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return "308143153:AAHd-ZTAqnzwExr9tYzEfxRMN0QzAVJlJCQ";
+        //Токен бота
+    }
+
+    @SuppressWarnings("deprecation") // Означает то, что в новых версиях метод уберут или заменят
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        System.out.println("msg.getChatId() =" + msg.getChatId());
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            sendMessage(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
---
+++ src/main/java/WorkWithRobot/TestClick.java
@@ -0,0 +1,72 @@
+package WorkWithRobot;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by a.shipulin on 21.02.18.
+ */
+public class TestClick {
+
+
+    public static void main(String[] args) {
+        TestClick click = new TestClick();
+        try {
+            /*click.click(2646, 515);*/
+            click.click(134, 1064); // будем запускать из панели быстрого запуска
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void click(int x, int y) throws AWTException, InterruptedException {
+
+        int TIME_CONST = 10000;
+        int DELAY_BEFORE = (int)(Math.random()*10000);
+        System.out.println("DELAY_BEFORE=" + DELAY_BEFORE);
+
+        Robot bot = new Robot();
+        Thread.sleep(DELAY_BEFORE);
+
+
+        bot.keyPress(KeyEvent.VK_WINDOWS);
+        bot.keyPress(KeyEvent.VK_D);
+        // Win+d is now pressed (receiving application should see a "key down" event.)
+        bot.keyRelease(KeyEvent.VK_D);
+        bot.keyRelease(KeyEvent.VK_WINDOWS);
+        // Win+d is now released (receiving application should now see a "key up" event - as well as a "key pressed" event).
+
+        /* снова разворачиваем все окна
+        Thread.sleep(TIME_CONST);
+        bot.keyPress(KeyEvent.VK_WINDOWS);
+        bot.keyPress(KeyEvent.VK_D);
+        // Win+d is now pressed (receiving application should see a "key down" event.)
+        bot.keyRelease(KeyEvent.VK_D);
+        bot.keyRelease(KeyEvent.VK_WINDOWS);
+        // Win+d is now released (receiving application should now see a "key up" event - as well as a "key pressed" event).*/
+
+        bot.mouseMove(x, y);
+        Thread.sleep(TIME_CONST);
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        /*
+        для панели быстрого запуска делаем один клик
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);*/
+        bot.mouseMove(950, 660);
+        Thread.sleep(TIME_CONST);
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        bot.mouseMove(1000, 650); // пока жмем нет
+        Thread.sleep(TIME_CONST);
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        Thread.sleep(TIME_CONST);
+        bot.mouseMove(1350, 817); //  закрываем его приблуду
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+
+    }
+
+}
