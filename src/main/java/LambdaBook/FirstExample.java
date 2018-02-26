63aeb0b7b20a5f7cf1e43217d3f11e5d54fbf875
a.shipulin <sfmrs@mail.ru>
2018-02-26 16:55:47 +0300

    Rejected commit: Default

---
+++ src/main/java/LambdaBook/FirstExample.java
@@ -0,0 +1,10 @@
+package LambdaBook;
+
+/**
+ * Created by a.shipulin on 26.02.18.
+ */
+public class FirstExample {
+    public static void main(String[] args) {
+        Runnable noArgument = () -> System.out.println("Hello World");
+    }
+}
